54d6c89c9c2cad2a68392615de9593f59c58b867
Hagwan Mohammed <79087273+Hagwan@users.noreply.github.com>
2024-07-05 16:16:13 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,392 @@
+import 'dart:io';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:lottie/lottie.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
+const String _apiKey = 'AIzaSyCXRGAXWZBLbkfmOrdbzE1T0PcA_Yhk_aI';
+
+void main() {
+  runApp(const GenerativeAISample());
+}
+
+class GenerativeAISample extends StatelessWidget {
+  const GenerativeAISample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 171, 222, 244),
+        ),
+        useMaterial3: true,
+      ),
+      home: FlutterSplashScreen(useImmersiveMode: true,
+          duration: const Duration(milliseconds: 2000),
+          nextScreen: const ChatScreen(title: 'Diagnosio 👨‍⚕️'),
+          backgroundColor: Colors.white,
+          splashScreenBody: Center(
+            child: Lottie.asset(
+              "assets/images/lottie_loading_2.json",
+              repeat: false,
+            ),
+          ),),
+    );
+  }
+}
+
+        
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        widget.title,
+        style: GoogleFonts.rubik(
+          textStyle: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        ),
+      )
+              .animate()
+              .fadeIn(duration: 900.ms, delay: 300.ms)
+              .shimmer(blendMode: BlendMode.srcOver, color: Colors.white12)
+              .move(begin: const Offset(-16, 0), curve: Curves.easeOutQuad)),
+      body: const ChatWidget(apiKey: _apiKey),
+    );
+  }
+}
+
+class ChatWidget extends StatefulWidget {
+  const ChatWidget({
+    required this.apiKey,
+    super.key,
+  });
+
+  final String apiKey;
+
+  @override
+  State<ChatWidget> createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode();
+  final List<({Image? image, String? text, bool fromUser})> _generatedContent =
+      <({Image? image, String? text, bool fromUser})>[];
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+        model: 'gemini-1.5-flash-latest',
+        apiKey: widget.apiKey,
+        systemInstruction: Content.system(
+            "You are an AI-powered chat model acts as a virtual doctor, offering preliminary assessments of symptoms and potential diagnoses. It provides personalized health insights, helping users identify possible conditions before consulting a real doctor. This tool aims to enhance patient awareness and expedite the medical consultation process, ensuring timely and informed healthcare decisions.") // Add the missing argument here
+
+        );
+    _chat = _model.startChat();
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textFieldDecoration = InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: _apiKey.isNotEmpty
+                ? ListView.builder(
+                    controller: _scrollController,
+                    itemBuilder: (context, idx) {
+                      final content = _generatedContent[idx];
+                      return MessageWidget(
+                        text: content.text,
+                        image: content.image,
+                        isFromUser: content.fromUser,
+                      );
+                    },
+                    itemCount: _generatedContent.length,
+                  )
+                : ListView(
+                    children: const [
+                      Text(
+                        'No API key found. Please provide an API Key using '
+                        "'--dart-define' to set the 'API_KEY' declaration.",
+                      ),
+                    ],
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 25,
+              horizontal: 15,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    autofocus: true,
+                    focusNode: _textFieldFocus,
+                    decoration: textFieldDecoration,
+                    controller: _textController,
+                    onSubmitted: _sendChatMessage,
+                  ),
+                ),
+                const SizedBox.square(dimension: 15),
+                IconButton(
+                  onPressed: !_loading
+                      ? () async {
+                          _sendImagePrompt(_textController.text);
+                        }
+                      : null,
+                  icon: Icon(
+                    Icons.image,
+                    color: _loading
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                if (!_loading)
+                  IconButton(
+                    onPressed: () async {
+                      _sendChatMessage(_textController.text);
+                    },
+                    icon: Icon(
+                      Icons.send,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  )
+                else
+                  const CircularProgressIndicator(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendImagePrompt(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      // Pick an image
+      final ImagePicker picker = ImagePicker();
+      final XFile? imageFile =
+          await picker.pickImage(source: ImageSource.gallery);
+
+      if (imageFile == null) {
+        // User canceled the picker
+        setState(() {
+          _loading = false;
+        });
+        return;
+      }
+
+      final byteData = await imageFile.readAsBytes();
+
+      final content = [
+        Content.multi([
+          TextPart(message),
+          // The only accepted mime types are image/*.
+          DataPart('image/jpeg', byteData),
+        ])
+      ];
+
+      _generatedContent.add((
+        image: Image.file(File(imageFile.path)),
+        text: message,
+        fromUser: true
+      ));
+
+      var response = await _model.generateContent(content);
+      var text = response.text;
+      _generatedContent.add((image: null, text: text, fromUser: false));
+
+      if (text == null) {
+        _showError('No response from API.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      _generatedContent.add((image: null, text: message, fromUser: true));
+      final response = await _chat.sendMessage(
+        Content.text(message),
+      );
+      final text = response.text;
+      _generatedContent.add((image: null, text: text, fromUser: false));
+
+      if (text == null) {
+        _showError('No response from API.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  void _showError(String message) {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: SelectableText(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    super.key,
+    this.image,
+    this.text,
+    required this.isFromUser,
+  });
+
+  final Image? image;
+  final String? text;
+  final bool isFromUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          isFromUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Container(
+                  constraints: const BoxConstraints(maxWidth: 520),
+                  decoration: BoxDecoration(
+                    color: isFromUser
+                        ? Theme.of(context).colorScheme.primaryContainer
+                        : Theme.of(context).colorScheme.inversePrimary,
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                  margin: const EdgeInsets.only(bottom: 8),
+                  child: Column(children: [
+                    if (text case final text?) MarkdownBody(data: text),
+                    if (image case final image?) image,
+                  ]))
+              .animate()
+              .fadeIn(duration: 900.ms, delay: 300.ms)
+              .move(begin: const Offset(-16, 0), curve: Curves.easeOutQuad),
+        ),
+      ],
+    );
+  }
+}
